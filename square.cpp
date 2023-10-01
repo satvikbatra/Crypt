b9a23bb4007a186dc18d5b6aab25a9b0d75a1f40
Satvik Batra <118475547+satvikbatra@users.noreply.github.com>
2023-10-01 20:20:00 +0530

    decrypt

--- square.cpp
+++ square.cpp
@@ -10,45 +10,53 @@ int sqrt(int n) {
     int start = 0;
     int end = n - 1;
     int ans;
-    long long mid = start + (end - start)/2;
+    long long mid;
 
     while(start <= end) {
-        if(mid*mid > n) {
+        mid = start + (end - start)/2;
+        if(mid * mid > n) {
             end = mid - 1;
         }
         else {
             ans = mid;
             start = mid + 1;
         }
-        mid = start + (end - start)/2;
     }
     return ans;
 }
 
 vector<vector<int>> storeInSquare(const vector<int>& bin) {
+
     int size = sqrt(bin.size()) + 1;
+
     vector<vector<int>> vect(size, vector<int>(size,0));
 
     int index = 0;
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
-            vect[i][j] = bin[index];
+            if(index < bin.size()) {
+                vect[i][j] = bin[index];
             index++;
+            } else {
+                vect[i][j] = 0;
+            }
         }
     }
+
     return vect;
 }
 
 vector<int> encryptBinaryString(const vector<vector<int>>& matrix) {
+
     vector <int> str;
     int size = matrix.size();
-    // int index = 0;
+
     for(int i = 0; i < size; i++) {
         for(int j = 0; j < size; j++) {
             str.push_back(matrix[i][j]);
-            // index++;
         }
     }
+
     return str;
 }
 
@@ -58,7 +66,7 @@ vector<vector<int>> encryptBySquare(const vector<vector<int>>& square) {
     // transpose of matrix
     vector<vector<int>> transposedMatrix = square;
 
-    for(int i = 0; i < size-1; i++) {
+    for(int i = 0; i < size - 1; i++) {
         for(int j = i+1; j < size; j++) {
             swap(transposedMatrix[i][j], transposedMatrix[j][i]);
         }
@@ -72,4 +80,23 @@ vector<vector<int>> encryptBySquare(const vector<vector<int>>& square) {
     }
 
     return reversedMatrix;
+}
+
+vector<vector<int>> decryptBySquare(const vector<vector<int>>& square) {
+    int size = square.size();
+
+    vector<vector<int>> reversedMatrix = square;
+    for(int i = 0; i < size; i++) {
+        reverse(reversedMatrix[i].begin(), reversedMatrix[i].end());
+    }
+
+    vector<vector<int>> originalMatrix = reversedMatrix;
+
+    for(int i = 0; i < size; i++) {
+        for(int j = i+1; j < size; j++) {
+            swap(originalMatrix[i][j], originalMatrix[j][i]);
+        }
+    }
+
+    return originalMatrix;
 }
